942f6a924516ca949d9efc0daf4c96fc0ccd88ea
Shimol-Khan <shimol@teleme.co>
2022-12-02 01:46:55 +0800

    Instance Types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-// 7 Primitive types
+// Instance Types
+let regexp: RegExp = new RegExp("ab+c");
+let array: Array<number> = [1, 2, 3];
+let set: Set<number> = new Set([1, 2, 3]);
 
-let isPresent: boolean = false;
-let magic: number = 77.9;
-let hello: string = "world";
-let notDefined: undefined = undefined;
-let notPresent: null = null;
-let penta: symbol = Symbol("star");
-let biggy: bigint = 24n;
+// A First in first out (FIFO) collection
+class Queue<T> {
+  private data: Array<T> = [];
+  push(item: T) {
+    this.data.push(item);
+  }
+  pop(): T | undefined {
+    return this.data.shift();
+  }
+}
+
+let queue: Queue<number> = new Queue();
