7837595f027ffe520ceeffb93ad02810b4e8d1df
Shimol-Khan <shimol@teleme.co>
2022-12-02 01:38:21 +0800

    Primitive Types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// 7 Primitive types
+
+let isPresent: boolean = false;
+let magic: number = 77.9;
+let hello: string = "world";
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+let penta: symbol = Symbol("star");
+let biggy: bigint = 24n;
